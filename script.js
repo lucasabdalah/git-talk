3e3ee8448072a4844e1abb94ce10c80bc9271462
lucasabdalah <lucasabdalah@gmail.com>
2022-04-23 20:12:50 -0300

    chore: adocaoAnimal

---
+++ script.js
@@ -0,0 +1,10 @@
+function adocaoAnimal(){
+    tipoAnimal = "doguinho"
+
+    if(tipoAnimal == "doguinho" || tipoAnimal == "cat"){
+        console.log('Eu adoto')
+    }else{
+        console.log('Eu não adoto')
+    }
+}
+adocaoAnimal()
